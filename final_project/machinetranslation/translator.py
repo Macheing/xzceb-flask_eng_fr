a61ad815c07d494cb8dff9a8cc13107a83e54c10
Macheing <macheingnyany@gmail.com>
2022-04-18 14:33:08 +0000

    added if conditions to catch empty string

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
-import json
+'''Needed libraries for this translations'''
+import os
+#import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,24 +16,33 @@ def translator_instance():
     # authenticate user
     authenticator = IAMAuthenticator(api_key)
     # create instance of service
-    language_translator = LanguageTranslatorV3(version='2021-08-01', 
-                                               authenticator=authenticator)
+    language = LanguageTranslatorV3(version='2021-08-01', authenticator=authenticator)
     # set service url to api url
-    language_translator.set_service_url(api_url)
-    return language_translator
+    language.set_service_url(api_url)
+    #language.set_disable_ssl_verification(True)
+    return language
 
 def english_to_french(english_text):
     '''translates english phrases to french'''
     # invokes translator instance
     language = translator_instance()
+    # checks when text is empty
+    if english_text == '' or english_text is None:
+        return ''
+    # else proceed with translatranslations
     # translates english text to french texts
-    french_text = language.translate(text=english_text, model_id='fr')
+    raw_text = language.translate(text=[english_text], model_id='en-fr').get_result()
+    french_text = raw_text.get('translations')[0]['translation']
     return french_text
 
 def french_to_english(french_text):
     '''translates french texts to english texts'''
     # invokes translation instance
     language = translator_instance()
+    if french_text =='' or french_text is None:
+        return ''
+    # else proceed with transltranslations.
     # translates from french to english
-    english_text = language.translate(text=french_text, model_id='en')
+    raw_text = language.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = raw_text.get('translations')[0]['translation']
     return english_text
